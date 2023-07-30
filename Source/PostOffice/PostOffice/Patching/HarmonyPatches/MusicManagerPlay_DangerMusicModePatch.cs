da805d4f92ee962973aea52b87c0c887eaa950a6
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2023-07-30 14:34:39 +0200

    some code comments

--- Source/PostOffice/PostOffice/Patching/HarmonyPatches/MusicManagerPlay_DangerMusicModePatch.cs
+++ Source/PostOffice/PostOffice/Patching/HarmonyPatches/MusicManagerPlay_DangerMusicModePatch.cs
@@ -15,23 +15,29 @@ public static class MusicManagerPlay_DangerMusicModePatch
 {
     public static bool Prefix(ref bool __result)
     {
-        if (Settings?.isActive is true && Settings.cai5000_delayCombatMusic)
+        // override base game implementation only iff mod is enabled
+        // and dependencies are loaded.
+        if (Settings is { isActive: true, cai5000_delayCombatMusic: true})
         {
+            // for every map check what the game thinks about the danger rating
             List<Map> maps = Find.Maps;
             for (int index = 0; index < maps.Count; ++index)
             {
                 if (maps[index].dangerWatcher.DangerRating == StoryDanger.High)
                 {
+                    // do some additional checks and only play combat music iff
+                    // enemies are *actually visible*
                     Map map = maps[index];
                     MapComponent_FogGrid? fog = map.GetComp_Fast<MapComponent_FogGrid>();
                     if (fog is not null)
                     {
-                        // TODO: maybe cache hostile pawns?
+                        // TODO: this could probably benefit from caching :P
                         __result = map.mapPawns.AllPawnsSpawned.Any(p => p.HostileTo(Faction.OfPlayerSilentFail) && !fog.IsFogged(p.Position));
                         Logger.LogVerbose($"DangerRating is high. RimWorld is attempting to play combat music! allowing combat music? {__result}");
                     }
                     else
                     {
+                        // if fog of war is disabled or otherwise unavailable, allow default behavior
                         __result = true;
                     }
                     return false;
@@ -40,6 +46,7 @@ public static class MusicManagerPlay_DangerMusicModePatch
             __result = false;
             return false;
         }
+        // forward to base game implementation
         return true;
     }
 }
